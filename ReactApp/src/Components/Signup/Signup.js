d3365107d3673d0073d7d18e833aea597818222f
Roshil Kapatel <roshil.170410116026@gmail.com>
2021-04-15 17:53:07 +0530

    Login and signup data is stored in state and hospital sign up is created

--- ReactApp/src/Components/Signup/Signup.js
+++ ReactApp/src/Components/Signup/Signup.js
@@ -1,35 +1,167 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Signup.css";
 
-export default class SignUp extends Component {
-    render() {
-        return (
-            <form>
-                <h3>Sign Up</h3>
+function Signup() {
+  const [activeTab, setactiveTab] = useState("Individual");
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [number, setnumber] = useState("");
 
-                <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
+  const handleTabchangeHospital = () => {
+    setactiveTab("Hospital");
+  };
 
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
+  const handleTabchangeIndividual = () => {
+    setactiveTab("Individual");
+  };
 
-                <div className="form-group">
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+  const handleSubmit = () => {
+    console.log(name, password, email);
+    setname("");
+    setemail("");
+    setpassword("");
+    setnumber("");
+  };
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
+  return (
+    <>
+      <div className="tabsContainerSignup">
+        <div className="tabIndividual">
+          {activeTab === "Individual" ? (
+            <div className="active">Individual</div>
+          ) : (
+            <div
+              onClick={handleTabchangeIndividual}
+              style={{ cursor: "pointer" }}
+            >
+              Individual
+            </div>
+          )}
+        </div>
+        <div className="tabHospital">
+          {activeTab === "Hospital" ? (
+            <div className="active">Hospital</div>
+          ) : (
+            <div
+              onClick={handleTabchangeHospital}
+              style={{ cursor: "pointer" }}
+            >
+              Hospital
+            </div>
+          )}
+        </div>
+      </div>
+      {activeTab === "Individual" ? (
+        <form>
+          <h3>Sign Up</h3>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
-                </p>
-            </form>
-        );
-    }
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              onChange={(e) => setname(e.target.value)}
+              value={name}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={(e) => setemail(e.target.value)}
+              value={email}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={(e) => setpassword(e.target.value)}
+              value={password}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-primary btn-block"
+            onClick={handleSubmit}
+          >
+            Sign Up
+          </button>
+          <p className="forgot-password text-right">
+            Already registered <a href="/sign-in">sign in?</a>
+          </p>
+        </form>
+      ) : (
+        <form>
+          <h3>Sign Up</h3>
+
+          <div className="form-group">
+            <label>Hospital Name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Hospital name"
+              onChange={(e) => setname(e.target.value)}
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            <label>Contact Number</label>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Number"
+              onChange={(e) => setnumber(e.target.value)}
+              value={number}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={(e) => setemail(e.target.value)}
+              value={email}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={(e) => setpassword(e.target.value)}
+              value={password}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-primary btn-block"
+            onClick={handleSubmit}
+          >
+            Sign Up
+          </button>
+          <p className="forgot-password text-right">
+            Already registered <Link to="/sign-in">sign in?</Link>
+          </p>
+        </form>
+      )}
+    </>
+  );
 }
+
+export default Signup;
