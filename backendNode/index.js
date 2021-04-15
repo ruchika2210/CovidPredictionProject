78daf2e63ef2d1d4d382fe4e92132b5de8da44f8
Roshil Kapatel <roshil.170410116026@gmail.com>
2021-04-16 00:10:57 +0530

    Model is working:

--- backendNode/index.js
+++ backendNode/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const port = process.env.PORT || 3000;
 require("./config/db.js");
-
+const Covid = require("./models/model");
 const app = express();
 app.use(express.static(`${__dirname}/uploads`));
 
 app.use("/upload", require("./routes/upload"));
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
+});
+
+app.get("/store", async (req, res) => {
+  const data = Covid({ name: "roshil" });
+  await data.save();
 });
