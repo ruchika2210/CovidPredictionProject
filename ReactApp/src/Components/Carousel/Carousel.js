6cc99e0e04d1c87ec215c062d2fbe89fcdcd9e09
ruchikagohil22 <ruchikagohil22@gmail.com>
2021-04-23 22:46:53 +0530

    Carousel modified

--- ReactApp/src/Components/Carousel/Carousel.js
+++ ReactApp/src/Components/Carousel/Carousel.js
@@ -4,43 +4,33 @@ import { Carousel } from "react-bootstrap";
 function Carouselhome() {
   return (
     <div>
-      <Carousel>
-        <Carousel.Item interval={1000}>
+      <Carousel fade>
+        <Carousel.Item>
           <img
             className="d-block w-100"
             src="https://theindianpractitioner.com/wp-content/uploads/2020/07/Formulation-Based-on-Natural-Molecules-to-Combat-COVID-19.jpg"
             height="500px"
-            width="150px"
+            width="140px"
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={500}>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
+            src="https://production-tcf.imgix.net/app/uploads/2020/09/02133407/halting-workplace-01.png?auto=format%2Ccompress&q=80&fit=crop&w=1200&h=600"
+            height="500px"
+            width="150px"
             alt="Second slide"
           />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
+            src="https://ohsonline.com/-/media/OHS/OHS/Images/2020/12/kusti.jpg"
+            height="500px"
+            width="140px"
             alt="Third slide"
           />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </div>
